fbc34d8eae186a5125bd61257a582656514ab41a
valeriia rieznikova <72576722+valeria-rezn@users.noreply.github.com>
2023-04-27 10:48:14 +0300

    added enum ItemType for switch

--- cpp/src/GildedRose.h
+++ cpp/src/GildedRose.h
@@ -16,13 +16,23 @@ public:
 class GildedRose
 {
 public:
+   
     vector<Item>& items;
     GildedRose(vector<Item>& items);
-
+    enum ItemType
+    {
+        ITEM_NORMAL,
+        ITEM_AGED_BRIE,
+        ITEM_SULFURAS,
+        ITEM_BACKSTAGE_PASSES,
+        ITEM_CONJURED
+    };
     void updateQuality();
     void updateQualityAgedBrie(int index);
     void updateQualityBackstagePasses(int index);
     void updateQualityConjured(int index);
     void updateQualitySulfuras(int index);
     void updateQualityNormal(int index);
+private:
+    ItemType getItemTypeByName(const string& name);
 };
