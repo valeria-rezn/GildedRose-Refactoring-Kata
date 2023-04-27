4d17b6090c93d401c0c1fc909237e53aa4178547
valeriia rieznikova <72576722+valeria-rezn@users.noreply.github.com>
2023-04-27 10:47:11 +0300

    added constant string and switch

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -1,72 +1,102 @@
 #include "GildedRose.h"
+#include "iostream"
+
+const int MIN_QUALITY = 0;
+const int MAX_QUALITY = 50;
+const string AG_BR = "Aged Brie";
+const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+const string BACKST_PS = "Backstage passes to a TAFKAL80ETC concert";
+const string CONJURED = "Conjured Mana Cake";
+
 
 GildedRose::GildedRose(vector<Item>& items) : items(items)
-{
+{}
 
+GildedRose::ItemType GildedRose::getItemTypeByName(const string& name)
+{
+    if (name == AG_BR) {
+        return ITEM_AGED_BRIE;
+    }
+    else if (name == SULFURAS) {
+        return ITEM_SULFURAS;
+    }
+    else if (name == BACKST_PS) {
+        return ITEM_BACKSTAGE_PASSES;
+    }
+    else if (name == CONJURED) {
+        return ITEM_CONJURED;
+    }
+    else {
+        return ITEM_NORMAL;
+    }
 }
 
 void GildedRose::updateQualityAgedBrie(int index)
 {
-    if (items[index].quality < 50)
+    if (items[index].quality < MAX_QUALITY)
     {
         items[index].quality += 1;
     }
-    items[index].sellIn -= 1;
-    if (items[index].sellIn < 0 && items[index].quality<50)
+    items[index].sellIn--;
+    if (items[index].sellIn < 0 && items[index].quality< MAX_QUALITY)
     {
         items[index].quality += 1;
     }
 }
+
 void GildedRose::updateQualityBackstagePasses(int index)
 {
-    if (items[index].quality < 50)
+    if (items[index].quality < MAX_QUALITY)
     {
         items[index].quality += 1;
 
-        if (items[index].sellIn <= 10 && items[index].quality < 50)
+        if (items[index].sellIn <= 10 && items[index].quality < MAX_QUALITY)
         {
             items[index].quality += 1;
         }
-        if (items[index].sellIn <= 5 && items[index].quality < 50)
+        if (items[index].sellIn <= 5 && items[index].quality < MAX_QUALITY)
         {
             items[index].quality += 1;
         }
     }
-    items[index].sellIn -= 1;
+    items[index].sellIn--;
     if (items[index].sellIn < 0)
     {
         items[index].quality = 0;
     }
     
 }
+
 void GildedRose::updateQualitySulfuras(int index)
 {
 }
+
 void GildedRose::updateQualityConjured(int index)
 {
-    if (items[index].quality > 0)
+    if (items[index].quality > MIN_QUALITY)
     {
         items[index].quality -= 2;
     }
-    items[index].sellIn -= 1;
-    if (items[index].sellIn < 0 && items[index].quality>0)
+    items[index].sellIn--;
+    if (items[index].sellIn < 0 && items[index].quality>MIN_QUALITY)
     {
         items[index].quality -= 2;
     }
-    if (items[index].quality < 0)
+    if (items[index].quality < MIN_QUALITY)
     {
         items[index].quality = 0;
     }
 }
+
 void GildedRose::updateQualityNormal(int index)
 {
-    if (items[index].quality > 0)
+    if (items[index].quality > MIN_QUALITY)
     {
         items[index].quality -= 1;
        
     }
-    items[index].sellIn -= 1;
-    if (items[index].sellIn < 0 && items[index].quality>0)
+    items[index].sellIn--;
+    if (items[index].sellIn < 0 && items[index].quality>MIN_QUALITY)
     {
         items[index].quality -= 1;
     }
@@ -76,17 +106,25 @@ void GildedRose::updateQuality()
 {
     for (int itemIndex = 0; itemIndex < items.size(); itemIndex++)
     {
-
-        if (items[itemIndex].name == "Aged Brie")
+        ItemType itemType = getItemTypeByName(items[itemIndex].name);
+        switch (itemType) 
+        {
+        case ITEM_AGED_BRIE:
             updateQualityAgedBrie(itemIndex);
-        else if (items[itemIndex].name == "Backstage passes to a TAFKAL80ETC concert")
-            updateQualityBackstagePasses(itemIndex);
-        else if (items[itemIndex].name == "Sulfuras, Hand of Ragnaros")
+            break;
+        case ITEM_SULFURAS:
             updateQualitySulfuras(itemIndex);
-        else if (items[itemIndex].name == "Conjured Mana Cake")
+            break;
+        case ITEM_BACKSTAGE_PASSES:
+            updateQualityBackstagePasses(itemIndex);
+            break;
+        case ITEM_CONJURED:
             updateQualityConjured(itemIndex);
-        else
+            break;
+        case ITEM_NORMAL:
             updateQualityNormal(itemIndex);
-        
+            break;
+        }
     }
 }
+
